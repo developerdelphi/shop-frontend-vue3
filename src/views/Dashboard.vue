f1f62dd877c705a637c6cf34b094a8fe6badb794
Oleomar Buchner <developerdelphi@gmail.com>
2022-01-30 16:25:27 -0300

    Create View Dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <DefaultLayout>
+    <h1 class="title-page">Dashboard</h1>
+  </DefaultLayout>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+import DefaultLayout from '../components/layouts/DefaultLayout.vue'
+export default defineComponent({
+  name: 'Dashboard',
+  components: {DefaultLayout}
+})
+</script>
