ac0c3bd22a5cbd71f4533c3e21af98ee7336b962
Oleomar Buchner <developerdelphi@gmail.com>
2022-01-30 10:17:40 -0300

    Auth: Ensure signin request api and update authState

--- src/services/auth/SignIn.ts
+++ src/services/auth/SignIn.ts
@@ -1,16 +1,30 @@
 import Api from '../api';
+import useAuthState from '../../store/auth/useAuthState';
+
 type SignInDataRequest = {
   email: string;
   password: string;
 };
 
-async function SignInRequest(data: SignInDataRequest) {
+const authState = useAuthState();
+
+async function SignInRequest(data: SignInDataRequest): Promise<any> {
   try {
-    Api.post('api/signin', data).then((response) => {
-      console.log(response.data);
+    await Api.post('api/signin', data).then((response) => {
+      const { name, email, created_at } = response.data.user;
+      authState.authenticate({ name, email, created_at });
     });
-  } catch (error) {
-    console.log(error.response.data);
+  } catch (error: any) {
+    if (error.response.status === 422 || error.response.status === 401) {
+      if (error.response.data.errors) {
+        const { email, password, message } = error.response.data.errors;
+        authState.setErrors({ email, password, message });
+      }
+      if (error.response.data.message) {
+        const { message } = error.response.data;
+        authState.setErrors({ email: [], password: [], message });
+      }
+    }
   }
 }
 
