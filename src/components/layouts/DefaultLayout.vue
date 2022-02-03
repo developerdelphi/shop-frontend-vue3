c0b0a53ce93c5e0cce5d093b724bef4b5b4981d3
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-03 16:31:25 -0300

    ensure footer preserve in bottom

--- src/components/layouts/DefaultLayout.vue
+++ src/components/layouts/DefaultLayout.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="bg-stone-800 min-h-screen flex flex-col">
     <NavBar />
-    <main class="p-2">
+    <main class="p-2 flex-1">
       <slot />
     </main>
     <footer
-      class="absolute px-2 inset-x-0 bg-stone-900 bottom-0 text-sm py-1 flex flex-row justify-center"
+      class="px-2 inset-x-0 bg-stone-900 bottom-0 text-sm py-1 flex flex-row justify-center left-0"
     >
-      <p class="font-thin text-xs">Agência Be on the web - All Rigths Reserveds &nbsp;2022</p>
+      <p class="font-thin text-xs">
+        <a href="https://beontheweb.com.br" target="_blank">Agência Be on the web</a> - All Rigths Reserveds &nbsp;2022
+      </p>
     </footer>
   </div>
 </template>
