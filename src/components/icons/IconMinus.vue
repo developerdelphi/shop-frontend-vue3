d2737717b5160c19ec8d15ccb43f536a2a05c7df
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-03 15:52:05 -0300

    Criado icones Minus e Plus

---
+++ src/components/icons/IconMinus.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="transition ease-in-out delay-200 hover:scale-105 hover:text-orange-500"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4" />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+name: 'IconMinus'
+</script>
+
+<style>
+</style>
---
+++ src/components/icons/IconPlus.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="transition ease-in-out delay-200 hover:scale-105 hover:text-orange-500"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+  name: 'IconPlus'
+</script>
+
+<style>
+</style>
