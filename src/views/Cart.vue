451389fdf0e6787181bf9e0ed9216c92418627bd
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-03 11:37:31 -0300

    Cart - Create View

---
+++ src/views/Cart.vue
@@ -0,0 +1,18 @@
+<template>
+  <DefaultLayout>
+    <h1 class="title-page">Carrinho</h1>
+  </DefaultLayout>
+</template>
+
+<script lang="ts">
+import { defineComponent} from 'vue'
+import DefaultLayout from '../components/layouts/DefaultLayout.vue';
+
+export default defineComponent({
+  name: 'CartPage',
+  components: {DefaultLayout}
+});
+</script>
+
+<style>
+</style>
