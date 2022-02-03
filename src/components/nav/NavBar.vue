4c1c2ed2c216b9ceb1dc764425fc3906c7410046
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-03 11:59:04 -0300

    Navbar add link to cart

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -3,56 +3,42 @@
     <div class="flex flex-row justify-between items-center w-full p-2">
       <div class>
         <a href="http://localhost:3000/">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-8 w-8"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <icon-brand class="h-8 w-8" />
         </a>
       </div>
-      <div></div>
-      <div>
+      <div class="flex flex-1 justify-end pr-4">
+        <router-link :to="{ name: 'cart' }">
+          <div class="inline-flex">
+            <icon-cart
+              class="h-6 w-6 transform ease-in-out duration-200 hover:text-orange-500 hover:scale-105"
+            />
+            <span class="text-xl text-orange-400">{{ countItensCart() }}</span>
+          </div>
+        </router-link>
+      </div>
+      <div class="z-50">
         <span
           @click="toggleMenu = !toggleMenu"
           class="transform ease-in-out duration-300 hover:scale-110 hover:text-orange-500"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <icon-menu class="h-6 w-5" />
         </span>
       </div>
     </div>
-    <div
-      v-if="toggleMenu"
-      class="flex flex-row justify-end bg-stone-400 w-3/4 right-0 absolute top-12 p-2 shadow-2xl shadow-slate-400 pr-4 text-lg"
-    >
-      <router-link :to="{name: 'signin'}" v-if="!isAuthenticated">Login</router-link>
+    <div v-if="toggleMenu" class="menu-toggle">
+      <router-link :to="{ name: 'signin' }" v-if="!isAuthenticated">Login</router-link>
       <div v-else class="text-right w-full">
         <div class="text-stone-500 font-semibold flex flex-col">
           <span>{{ user.name }}</span>
           <span class="text-sm">{{ user.email }}</span>
         </div>
-        <div class="hover:text-stone-800 mt-2 border-b border-stone-500/30 w-full">Sair</div>
+        <div class="menu mt-2 pb-1 border-b border-stone-500/30 w-full">
+          <a href="#">Sair</a>
+        </div>
         <div class="menu flex flex-col w-full">
-          <router-link :to="{name: 'home'}">Home</router-link>
-          <router-link :to="{name: 'dashboard'}">Meus pedidos</router-link>
-          <router-link :to="{name: 'products'}">Produtos</router-link>
+          <router-link :to="{ name: 'home' }">Home</router-link>
+          <router-link :to="{ name: 'dashboard' }">Meus pedidos</router-link>
+          <router-link :to="{ name: 'products' }">Produtos</router-link>
         </div>
       </div>
     </div>
@@ -62,16 +48,35 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import useAuthState from '../../store/auth/useAuthState';
+import useCartState from '../../store/useCartState';
+
+import IconCart from '../icons/IconCart.vue';
+import IconBrand from '../icons/IconBrand.vue';
+import IconMenu from '../icons/IconMenu.vue';
 
 export default defineComponent({
   name: 'NavBar',
+  components: { IconBrand, IconCart, IconMenu },
 
   setup() {
-    const toggleMenu = ref<boolean>(false)
+    const toggleMenu = ref<boolean>(false);
 
-    const {isAuthenticated, state} = useAuthState()
+    const { isAuthenticated, state } = useAuthState();
+    const { state: cartStore, countItensCart } = useCartState();
 
-    return {toggleMenu, isAuthenticated, user: state.user}
+    return {
+      toggleMenu,
+      isAuthenticated,
+      user: state.user,
+      cartStore,
+      countItensCart
+    };
   }
-})
+});
 </script>
+
+<style scope>
+  .menu-toggle {
+    @apply flex flex-row justify-end bg-stone-400 w-3/4 md:w-1/2 right-0 absolute top-12 p-2 shadow-2xl shadow-orange-100/20 pr-4 text-lg;
+  }
+</style>
