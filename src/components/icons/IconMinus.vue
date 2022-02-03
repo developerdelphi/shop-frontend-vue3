5da1a1190386a1c802f20c14996026a482aee90e
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-03 16:15:57 -0300

    Criado icone Trash

--- src/components/icons/IconMinus.vue
+++ src/components/icons/IconMinus.vue
@@ -11,7 +11,6 @@
 </template>
 
 <script lang="ts" setup>
-name: 'IconMinus'
 </script>
 
 <style>
--- src/components/icons/IconPlus.vue
+++ src/components/icons/IconPlus.vue
@@ -11,7 +11,6 @@
 </template>
 
 <script lang="ts" setup>
-  name: 'IconPlus'
 </script>
 
 <style>
