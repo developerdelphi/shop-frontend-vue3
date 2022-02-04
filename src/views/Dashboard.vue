220456c1173fd47d6dc8cbe478da275c77bac5d7
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-04 11:07:45 -0300

    import service useUser to component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,25 @@
 <template>
   <DefaultLayout>
     <h1 class="title-page">Dashboard</h1>
+    <div class="text-stone-100">{{ user }}</div>
   </DefaultLayout>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue'
 import DefaultLayout from '../components/layouts/DefaultLayout.vue'
+import useUser from '../services/auth/useUser';
+
 export default defineComponent({
   name: 'Dashboard',
-  components: {DefaultLayout}
+  components: {DefaultLayout},
+
+  setup() {
+    const {getUser, user} = useUser()
+
+    getUser()
+
+    return {user}
+  }
 })
 </script>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -19,7 +19,7 @@
         </svg>
       </span>
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 z-auto mt-4">
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 z-auto mt-4 px-2">
       <div
         v-for="product in products"
         :key="product.id"
