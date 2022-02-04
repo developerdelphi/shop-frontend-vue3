bc5df0a32e9f56e4ab275b65855c5623871fe299
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-04 11:07:08 -0300

    Auth - Create service useUser

---
+++ src/services/auth/useUser.ts
@@ -0,0 +1,19 @@
+import { reactive, toRefs } from 'vue'
+import Api from '../api'
+
+const state = reactive({
+  user: {}
+})
+
+async function getUser() {
+  try {
+    await Api.get('api/user/').then(({ data }) => (state.user = data))
+  } catch (error: any) {
+    console.error(error.response.data)
+  }
+}
+
+export default () => ({
+  getUser,
+  ...toRefs(state)
+})
