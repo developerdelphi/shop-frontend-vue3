67ab792c1b8a0ffa7851e74515ba3fafebfa12bc
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-04 15:35:02 -0300

    Api - Axios get response 401 not procedure add

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,26 +1,35 @@
-import axios from 'axios';
+import axios from 'axios'
+import useAuthState from '../store/auth/useAuthState'
+
+const useAuth = useAuthState()
 
 const Api = axios.create({
   baseURL: 'http://localhost:8000/',
   headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-  withCredentials: true,
-});
+  withCredentials: true
+})
 
 Api.interceptors.response.use(
-  (response) => response,
-  async (err) => {
-    const status = err.response.status;
+  response => response,
+  async err => {
+    const status = err.response.status
 
     if (status === 419) {
       // Refresh our session token
-      await Api.get('sanctum/csrf-cookie');
+      await Api.get('sanctum/csrf-cookie')
 
       // Return a new request using the original request's configuration
-      return Api(err.response.config);
+      return Api(err.response.config)
     }
 
-    return Promise.reject(err);
-  }
-);
+    if (status === 401) {
+      console.error('ERRO 401 CAPTURADO')
+      // useAuth.authenticate({ name: '', email: '', created_at: '' })
+      // router.push({ name: 'signin' })
+    }
 
-export default Api;
+    return Promise.reject(err)
+  }
+)
+
+export default Api
