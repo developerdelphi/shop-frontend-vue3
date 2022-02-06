dc659126969d9fe4e8582a5410a8685369f98ed4
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-06 16:49:42 -0300

    Dashboard: Show Orders in view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,24 +2,41 @@
   <DefaultLayout>
     <h1 class="title-page">Dashboard</h1>
     <div class="text-stone-100">{{ user }}</div>
+    <hr />
+    <div class="text-stone-100">
+      <h2>Pedidos</h2>
+      <div
+        v-for="order in orders"
+        :key="order.id"
+        class="grid grid-cols-4 shadow border border-transparent p-2 card"
+      >
+        <div>Pedido: #{{ order.id }}</div>
+        <div>{{ order.created_at }}</div>
+        <div>Total: {{ new Intl.NumberFormat('pt-BR', {currency: 'BRL', style:'currency'}).format(order.total) }}</div>
+        <div>Status: {{order.status }}</div>
+      </div>
+    </div>
   </DefaultLayout>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue'
-import DefaultLayout from '../components/layouts/DefaultLayout.vue'
-import useUser from '../services/auth/useUser';
+import DefaultLayout from '@/components/layouts/DefaultLayout.vue'
+import useUser from '@/services/auth/useUser';
 
 export default defineComponent({
   name: 'Dashboard',
   components: {DefaultLayout},
 
   setup() {
-    const { getUser, user } = useUser()
+    const { getUser, requestOrders, getOrders, user } = useUser()
 
     getUser()
 
-    return { user }
+    getOrders()
+    requestOrders()
+
+    return { user, orders: getOrders() }
   }
 })
 </script>
