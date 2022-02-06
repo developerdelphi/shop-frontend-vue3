cb90389448ca1291e3949787d2efcc8d1f214a8b
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-06 16:49:02 -0300

    UserService - modified methor getUser to actions and getters

--- src/services/auth/useUser.ts
+++ src/services/auth/useUser.ts
@@ -1,19 +1,38 @@
-import { reactive, toRefs } from 'vue'
-import Api from '../api'
+import { computed, reactive, toRefs } from 'vue'
+import Api from '@/services/api'
 
 const state = reactive({
-  user: {}
+  user: {},
+  orders: { data: [] }
 })
 
-async function getUser() {
-  try {
-    await Api.get('api/user/').then(({ data }) => (state.user = data))
-  } catch (error: any) {
-    // console.error(error.response.data.errors)
+const getters = {
+  getOrders: () => computed(() => state.orders.data)
+}
+
+const actions = {
+  getUser: async () => {
+    try {
+      await Api.get('api/user/').then(({ data }) => (state.user = data))
+    } catch (error: any) {
+      // console.error(error.response.data.errors)
+    }
+  },
+
+  requestOrders: async () => {
+    try {
+      await Api.get('api/orders/user').then(response => {
+        // console.log('LISTA DE ORDERS', response.data)
+        state.orders = response.data
+      })
+    } catch (error: any) {
+      console.error(error.respose.data)
+    }
   }
 }
 
 export default () => ({
-  getUser,
+  ...actions,
+  ...getters,
   ...toRefs(state)
 })
