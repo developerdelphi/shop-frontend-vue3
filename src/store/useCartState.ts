c744e399389bf26e54398ce9e52fab96e8195a03
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-06 10:55:13 -0300

    Cart - create action reset and update response getter count total

--- src/store/useCartState.ts
+++ src/store/useCartState.ts
@@ -1,38 +1,38 @@
-import { computed, reactive, readonly, watch } from 'vue';
+import { computed, reactive, readonly, watch } from 'vue'
 
 interface ItemCart extends addItemCart {
-  quantity: number;
+  quantity: number
 }
 
 type addItemCart = {
-  id: number;
-  name: string;
-  price: number;
-};
+  id: number
+  name: string
+  price: number
+}
 
-const STATE_NAME = 'SHOP-CART';
+const STATE_NAME = 'SHOP-CART'
 
 const defaultState = {
   items: [],
-  cartView: false,
-};
+  cartView: false
+}
 
 const getDefaultState = () => {
   if (localStorage.getItem(STATE_NAME) !== null) {
-    return JSON.parse(localStorage.getItem(STATE_NAME));
+    return JSON.parse(localStorage.getItem(STATE_NAME))
   }
-  return defaultState;
-};
+  return defaultState
+}
 
-let state = reactive(getDefaultState());
+let state = reactive(getDefaultState())
 
 const getters = {
   getItems: () =>
     computed(() => {
-      return state.items;
+      return state.items
     }),
   getCartView: () => {
-    return !!state.cartView;
+    return !!state.cartView
   },
 
   countItensCart: () => state.items.length,
@@ -40,79 +40,84 @@ const getters = {
   countQuantityCart: () =>
     computed(() =>
       state.items.reduce((total: number, item: ItemCart) => {
-        return total + item.quantity;
+        return total + item.quantity
       }, 0)
     ),
 
-  getTotal: computed(() =>
-    state.items.reduce((total: number, item: ItemCart) => {
-      return total + item.quantity * item.price;
+  getTotal: computed(() => {
+    let value: number
+    value = state.items.reduce((total: number, item: ItemCart) => {
+      return total + item.quantity * item.price
     }, 0)
-  ),
-};
+    return value
+  })
+}
 const actions = {
   setCartView: () => {
-    state.cartView = !state.cartView;
+    state.cartView = !state.cartView
   },
 
   addItemToCart: ({ id, name, price }: addItemCart) => {
-    const existItem = state.items.find((item: ItemCart) => item.id === id);
+    const existItem = state.items.find((item: ItemCart) => item.id === id)
 
-    if (!existItem) return state.items.push({ id, name, price, quantity: 1 });
+    if (!existItem) return state.items.push({ id, name, price, quantity: 1 })
 
     const updateItens: ItemCart[] = state.items.map((item: ItemCart) => {
       if (item.id === id) {
-        item.quantity++;
+        item.quantity++
       }
-      return item;
-    });
-    state.items = updateItens;
-    return;
+      return item
+    })
+    state.items = updateItens
+    return
   },
 
   removeItemToCart: (id: number) => {
-    const existItem = state.items.find((item: ItemCart) => item.id === id);
+    const existItem = state.items.find((item: ItemCart) => item.id === id)
 
     if (existItem) {
-      const updateItens = state.items.filter(
-        (item: ItemCart) => item.id !== id
-      );
-      state.items = updateItens;
+      const updateItens = state.items.filter((item: ItemCart) => item.id !== id)
+      state.items = updateItens
     }
-    return;
+    return
   },
 
   decItemCart: (id: number) => {
-    const existItem = state.items.find((item: ItemCart) => item.id === id);
+    const existItem = state.items.find((item: ItemCart) => item.id === id)
 
-    if (!existItem) return;
+    if (!existItem) return
 
     const updateItens = state.items.map((item: ItemCart) => {
-      if (item.id === id) item.quantity--;
-      return item;
-    });
+      if (item.id === id) item.quantity--
+      return item
+    })
 
-    state.items = updateItens.filter((item: ItemCart) => item.quantity >= 1);
+    state.items = updateItens.filter((item: ItemCart) => item.quantity >= 1)
   },
-};
+
+  reset: () => {
+    state.items = []
+    state.cartView = false
+  }
+}
 
 watch(
   () => state,
   () => {
-    localStorage.setItem(STATE_NAME, JSON.stringify(state));
+    localStorage.setItem(STATE_NAME, JSON.stringify(state))
   },
   {
-    deep: true,
+    deep: true
   }
-);
+)
 
 export default () => {
   if (localStorage.getItem(STATE_NAME) === null) {
-    localStorage.setItem(STATE_NAME, JSON.stringify(state));
+    localStorage.setItem(STATE_NAME, JSON.stringify(state))
   }
   return {
     state: readonly(state),
     ...actions,
-    ...getters,
-  };
-};
+    ...getters
+  }
+}
