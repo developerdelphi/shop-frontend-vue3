a83a2293e655b83ae80f95bc71e0a1a5f5fdfaa0
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-10 10:49:20 -0300

    Types - Create types for auth State

---
+++ src/types/auth/authTypes.ts
@@ -0,0 +1,25 @@
+type userDataType = {
+  name: string
+  email: string
+  created_at: string
+}
+
+type errorsDataType = {
+  email?: Array<string>
+  password?: Array<string>
+  message?: string
+}
+
+interface IStateAuth {
+  user: userDataType
+  isAuthenticated: boolean
+  errors: errorsDataType
+  token?: string
+}
+
+interface IAuthData {
+  user: userDataType
+  token: string
+}
+
+export { userDataType, errorsDataType, IStateAuth, IAuthData }
